a7b9fc09b5e94f089bd2f740c68e39307ac3f741
AlphaRisqi <habibalfarisqi18@gmail.com>
2026-06-04 20:43:31 +0700

    Finished Project #1

--- js/gb.js
+++ js/gb.js
@@ -3,12 +3,16 @@ const anchor = document.getElementById('explore');
 const disco = document.getElementById('disco');
 const link = document.querySelectorAll(".link");
 const list = document.querySelectorAll(".list");
+const gb = document.getElementById('gb');
+const summarycontent = document.getElementById('summarycontent');
+const after = document.getElementById('aftersubmit')
 const options = {
   root: null,
   threshold: 0,
   rootMargin: "0px"
 };
 
+// Header
 const headerObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
@@ -26,6 +30,38 @@ const headerObserver = new IntersectionObserver((entries) => {
 }, options);
 
 headerObserver.observe(anchor);
+
+// GB Summary
+gb.addEventListener('submit', function(event) {
+  alert("Thank you for submitting the guest book");
+
+  after.classList.remove('gb');
+  after.classList.add('gb' ,'aftersubmit');
+
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const name = form.get('fullname');
+  const email = form.get('email');
+  const work = form.get('work') ||  "Not Specified";
+  const exp = form.get('experience') || "-";
+
+  gb.reset();
+
+  summarycontent.classList.remove('gbplaceholder');
+  summarycontent.classList.add('summarycontent-active');
+
+  summarycontent.innerHTML = `
+    <div class="item"><strong>Name:</strong> ${escapeHTML(name)}</div>
+    <div class="item"><strong>Email:</strong> ${escapeHTML(email)}</div>
+    <div class="item"><strong>work:</strong> ${escapeHTML(work)}</div>
+    <div class="item"><strong>Your Experiences:</strong> ${escapeHTML(exp)}</div>
+  `;
+});
+
+function escapeHTML(str) {
+  return str.replace(/[&<>'"]/g, 
+  tag => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag));
+};
 
 const aboutme = document.getElementById('aboutme');
 const profile = document.getElementById('profile');
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const bg = [
 '../img/bg/10-things-to-do-in-yogyakarta-as-a-solo-traveler.jpg',
 '../img/bg/aerial-view-of-borobudur-temple-at-sunrise-near-yogyakarta-indonesia-video.jpg',
 '../img/bg/parangtritis_beach.jpg',
-// '../img/bg/',
+'../img/bg/Untitled-design-15.jpg',
 ];
 const bgslide = document.getElementById('bg');
 const options = {
@@ -16,7 +16,6 @@ const options = {
   rootMargin: "0px"
 };
 let indexnow = 0;
-
 function changeBG() {
   bgslide.style.backgroundImage = `url('${bg[indexnow]}')`;
   indexnow = (indexnow + 1) % bg.length;
@@ -43,3 +42,33 @@ const headerObserver = new IntersectionObserver((entries) => {
 
 headerObserver.observe(anchor);
 
+// faq
+const popupWindow = document.getElementById('popupbg');
+const popupText = document.getElementById('infotext');
+const closeButton = document.getElementById('infoclose');
+const triggerContainers = document.querySelectorAll('.faq');
+
+    triggerContainers.forEach(container => {
+      container.addEventListener('click', function() {
+        const specificInfoDiv = this.querySelector('.info');
+        popupText.innerHTML = specificInfoDiv.innerHTML;
+        popupWindow.classList.remove('popup-hidden');
+        popupWindow.classList.add('popup-visible');
+      });
+    });
+    
+    closeButton.addEventListener('click', function() {
+      popupWindow.classList.remove('popup-visible');
+      popupWindow.classList.add('popup-hidden');
+    });
+
+// aboutme
+const aboutme = document.getElementById('aboutme');
+const profile = document.getElementById('profile');
+const close = document.getElementById('close');
+aboutme.addEventListener('click', () =>{
+  profile.style.display = 'flex';
+});
+close.addEventListener('click', () =>{
+  profile.style.display = 'none';
+});
